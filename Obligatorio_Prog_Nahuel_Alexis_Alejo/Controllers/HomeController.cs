ca7d84dc6e811960e213f09fba8de970d89ee3ed
Nahuel Pages Oviedo <127339949+GuyaSuna@users.noreply.github.com>
2023-07-15 18:45:13 -0300

    Agregar archivos de proyecto.

---
+++ Obligatorio_Prog_Nahuel_Alexis_Alejo/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Obligatorio_Prog_Nahuel_Alexis_Alejo.Models;
+using System.Diagnostics;
+
+namespace Obligatorio_Prog_Nahuel_Alexis_Alejo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Obligatorio_Prog_Nahuel_Alexis_Alejo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Obligatorio_Prog_Nahuel_Alexis_Alejo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
